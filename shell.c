ce7f9e9a977ae7445d4f0c063c78dfd0bb53cf71
Nick Young <n.zachary.young@gmail.com>
2023-11-07 15:51:23 -0500

    'Bad Command' prints, shell starts at the end of the line rather than next line

--- shell.c
+++ shell.c
@@ -5,18 +5,19 @@
 int main()
 {
 	char* given_string;
-	char output_buffer;
+	char output_buffer[13312];
 	int sectorsRead;
-
 	while(1){
 		syscall(0, "C> ");
 		syscall(1, given_string);
 		syscall(3, given_string, output_buffer, &sectorsRead);
-
+		
+		// /*
 		if (sectorsRead == 0)
 			syscall(0, "Bad Command");
 		else
 			syscall(4, given_string);
+		// */
 
 		syscall(5);
 	}
