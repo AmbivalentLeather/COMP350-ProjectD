72a63023659a373b1d0d0bbe61e43b41559f6553
Nick Young <n.zachary.young@gmail.com>
2023-11-07 16:14:13 -0500

    Tried to work on step 5, couldn't get it working, I'll delete my changes and commit again

--- shell.c
+++ shell.c
@@ -10,6 +10,7 @@ int main()
 	while(1){
 		syscall(0, "C> ");
 		syscall(1, given_string);
+		syscall(0, given_string);
 		syscall(3, given_string, output_buffer, &sectorsRead);
 		
 		// /*
@@ -20,5 +21,28 @@ int main()
 		// */
 
 		syscall(5);
+	}
+}
+
+void commandChecker(char* pGiven_String)
+{
+	int i = 0;
+	char command[4];
+	char* type[4];
+	type[0] = "t";
+	type[1] = "y";
+	type[2] = "p";
+	type[3] = "e";
+
+	while(i < 4) {
+		command[i] = pGiven_String[i];
+		i++;
+	}
+	
+	for (i = 0; i < 4; i++) {
+		if(type[i] == command[i])
+			;
+		else
+			break;
 	}
 }
