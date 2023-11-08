6dd5fefcf46c4444cf0a401caea6e854ecc10fbd
Nick Young <n.zachary.young@gmail.com>
2023-11-07 21:26:14 -0500

    Type almost works perfectly but for the moment it prints both the error and the file contents

--- shell.c
+++ shell.c
@@ -9,7 +9,6 @@ int main()
 	char* given_string;
 	char testString[4];
 	char* cmdType = "type";
-
 	char* fileName;
 
 	int i = 0;
@@ -24,10 +23,9 @@ int main()
 		// Find, in the line given by the user, the filename to look for
 		// This is a temporary solution to a slightly larger problem
 		findFileName(given_string, fileName);
-		type(fileName);
 
-		if(testString == cmdType){
-			syscall(0, "raaaaaaaaaaa");
+		if (syscall(6, testString, cmdType)){
+			type(fileName);
 		}
 
 		syscall(5);
@@ -39,19 +37,19 @@ void type(char* inputFileName)
 	char buffer[13312]; /*this is the maximum size of a file*/
 	int sectorsRead;
 	syscall(3, inputFileName, buffer, &sectorsRead);
+
+	// /*
 	if (sectorsRead > 0)
 		syscall(0, buffer);
 	else
 		syscall(0, "File not found.\r\n");
+	// */
 }
 
 void findFileName(char* given_string, char* fileName)
 {
 	int i = 0;
-	while(i < 79) {
-		if(given_string[i + 5] == 0x0)
-			break;
-
+	while(i < 12) {
 		fileName[i] = given_string[i + 5];
 		++i;
 	}
