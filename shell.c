b07613d1b2fca9289af6380f8c7a4076f2ac0ce9
Nick Young <n.zachary.young@gmail.com>
2023-11-16 14:18:59 -0500

    Substituted old findCommand and findFileName functions in shell.c with argFinder, a better system for later on

--- shell.c
+++ shell.c
@@ -4,9 +4,9 @@
  */
 void type(char* inputFileName);
 void exec(char* inputFileName);
-void findFileName(char userInput[], char* fileName);
-void findCommandName(char inputString[], char* outputString);
 int stringCompare(char* given, char* compared_to);
+void argFinder(char*, char*, int);
+void numOfArgs(char*, int*);
 
 int main()
 {
@@ -21,21 +21,21 @@ int main()
 		syscall(0, "\rC> ");
 		syscall(1, userInput);
 	
-		// Find the command used and the file name being referenced
-		findCommandName(userInput, cmdString);
-		findFileName(userInput, fileName);
+		argFinder(userInput, cmdString, 0);
+		// NOTE: I want to eventually use numOfArgs to report an error if the user
+		// enters more arguments than are expected of the command
 
-//		/*
 		if (stringCompare(cmdString, cmdType)) {
+			argFinder(userInput, fileName, 1);
 			type(fileName);
 		}
 		else if (stringCompare(cmdString, cmdExec)) {
+			argFinder(userInput, fileName, 1);
 			exec(fileName);
 		}
 		else {
 			syscall(0, "Bad command!\n\r");
 		}
-		// */
 		
 	}
 		
@@ -47,15 +47,10 @@ void type(char* inputFileName)
 	int sectorsRead;
 	syscall(3, inputFileName, buffer, &sectorsRead);
 
-	// /*
-	if (sectorsRead > 0) {
+	if (sectorsRead > 0)
 		syscall(0, buffer);
-	}	
-	else {
+	else
 		syscall(0, "File not found.\r\n");
-	}
-	// */
-
 }
 
 void exec(char* inputFileName)
@@ -65,35 +60,13 @@ void exec(char* inputFileName)
 	int sectorsRead;
 	syscall(3, inputFileName, buffer, &sectorsRead);
 
-	// /*
-	if (sectorsRead > 0) {
+	if (sectorsRead > 0)
 		syscall(4, inputFileName);
-	}	
-	else {
+	else
 		syscall(0, "File not found.\r\n");
-	}
-	// */
 }
 
-void findFileName(char* inputString, char* fileName)
-{
-	int i = 0;
-	while(i < 6) {
-		fileName[i] = inputString[i + 5];
-		i++;
-	}
-}
-
-void findCommandName(char inputString[], char* outputString)
-{
-	int i = 0;
-	while (inputString[i] != ' ') {
-		outputString[i] = inputString[i];
-		i++;
-	}
-	inputString[i] = '\0';
-}
-
+// I don't like that we're mixing [] and * since in this context they mean the same thing
 int stringCompare(char given[], char* compared_to)
 {
 	int i = 0;
@@ -106,14 +79,36 @@ int stringCompare(char given[], char* compared_to)
 	return 1;
 }
 
+void numOfArgs(char* input, int* args)
+{
+	int argumentIndex;
+	*args = 0;
+	for (argumentIndex = 0; input[argumentIndex] != '\0'; argumentIndex++)
+		if (input[argumentIndex] == ' ')
+			++*args;
+}
 
+void argFinder(char* input, char* output, int whichArg)
+{
+	// outputIndex is needed alongside inputIndex for this function to work
+	int argumentIndex = 0;
+	int outputIndex = 0;
+	int inputIndex = 0;
 
+	while (input[inputIndex] != '\0') {
+		if (input[inputIndex] == ' ') {
+			argumentIndex++;
+			inputIndex++;
+			continue;
+		}
 
+		if (whichArg == argumentIndex) {
+			output[outputIndex] = input[inputIndex];
+			outputIndex++;
+		}
 
+		inputIndex++;
+	}
 
-
-
-
-
-
-
+	output[outputIndex] = '\0';
+}
